adb13cfe2126783143b34b11cef621ec86408b00
VLR-angular7 <59470943+VLR-angular7@users.noreply.github.com>
2020-01-27 09:35:33 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Directives';
+  show:boolean=true;
+  employees:any[]=
+
+  [
+{name:"venkat",age:25,city:"hyd",salary:"100k",empImg:"https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/darbar-et00116717-07-11-2019-06-14-06.jpg"},
+{name:"srinivas",age:35,city:"pune",salary:"100k"},
+{name:"siva",age:36,city:"blr",salary:"900k"},
+{name:"udhay",age:39,city:"mum",salary:"90k"},
+{name:"sai",age:45,city:"hyd",salary:"170k"},
+
+
+  ]
+
+cricketTeams:any[]=[
+{teamName:"India",players:[{name:"Kohli",role:"batsman"},{name:"Rohith",role:"batsman"},{name:"Dhawan",role:"batsman"}]},
+{teamName:"Aus",players:[{name:"Warner",role:"batsman"},{name:"Rohith",role:"batsman"},{name:"Dhawan",role:"batsman"}]},
+{teamName:"England",players:[{name:"Root",role:"batsman"},{name:"Rohith",role:"batsman"},{name:"Dhawan",role:"batsman"}]},
+{teamName:"SA",players:[{name:"Dekock",role:"batsman"},{name:"Rohith",role:"batsman"},{name:"Dhawan",role:"batsman"}]},
+{teamName:"NZ",players:[{name:"Taylor",role:"batsman"},{name:"Rohith",role:"batsman"},{name:"Dhawan",role:"batsman"}]},
+
+]
+
+
+
+movies=[
+  "test1","test2"
+]
+
+
+
+
+
+
+
+}
